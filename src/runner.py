5c3ce0bf080312209fe251456405207d59b3ca60
Hudson Smith <hudson.smith.311@gmail.com>
2023-02-26 06:43:29 +0000

    Printing works.

--- src/runner.py
+++ src/runner.py
@@ -1,7 +1,11 @@
 class Runner(object):
     def __init__(self, script: str, filename: str) -> None:
         self.script: str = script
-        self.current_line: int = 0
+        
+        # Line number always starts at zero. (0th indexed arrays)
+        self.line_number: int = 0
+        self.current_line: str = ""
+
         self.filename: str = filename
 
         self.memory: dict = {}
@@ -12,17 +16,28 @@ class Runner(object):
         }
 
         self.operations: dict = {
-            "set": self.set
+            "set": self.set,
+            "print": self.print
         }
     
     def run(self) -> None:
         # Parse each line.
         for line in self.script:
-            op: str = line.split(" ")[0]
+            self.current_line = line
 
+            op: str = line.split(" ")[0].lower()
+            
             if op in list(self.operations.keys()):
                 self.operations[op](line)
-                self.current_line += 1
+            
+            else:
+                self.error(f"Undefined operation, {op.upper()}")
+
+            self.line_number += 1
+    
+    def error(self, message: str) -> None:
+        print(f"Runtime Error in {self.filename}:{self.line_number}\n\t{self.current_line}")
+        print(f"\n{message}")
 
     
     def set(self, line: str) -> None:
@@ -31,18 +46,28 @@ class Runner(object):
 
         # Clean it.
         for i, val in enumerate(parsed):
-            line[i] = val.lower().strip()
+            parsed[i] = val.lower().strip()
 
         var_name: str = parsed[1]
         var_type: str = parsed[2]
-        value = parsed[3]
+        value = parsed[4]
 
         try:
             self.memory[var_name] = self.types[var_type](value)
         
         except:
-            print(f"Runtime Error in {self.filename}:{self.current_line}:\n\t{line}")
-            print(f"\nIncompatible types for {var_type} and {value}")
+            self.error(f"Incompatible types for {var_type} and {value}")
+    
+    
+    def print(self, line: str) -> None:
+        parsed: list = line.split(" ", 1)
+
+        content: str = parsed[1]
+
+        # Check if the start and the ending of the content is a string.
+        if "\"" == content[0] and "\"" == content[-1] or "\'" == content[0] and "\'" == content[-1]:
+            print(content[1:-1])
+
         
     
 
